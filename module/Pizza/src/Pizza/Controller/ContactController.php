1e0c6482e4da47ae6006fa1cf1f9712aafb131df
anassrakibi <thegentletrainer@gmail.com>
2014-09-13 13:08:01 +0200

    add the contact controller

---
+++ module/Pizza/src/Pizza/Controller/ContactController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Pizza\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Pizza\Form\ContactForm;
+class ContactController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        //return new ViewModel();
+
+        $contact_form = new ContactForm();
+        $request  = $this->getRequest();
+        if($request->isPost())
+        	{  
+        		$message = new \Zend\Mail\Message();
+        		$message->setBody($_POST['message']);
+        		$message->setFrom($_POST['email_adress']);
+        		$message->setSubject(" contact message ");
+        		$message->addTo('thegentletrainer@gmail.com');
+
+        		$smtpOptions = new \Zend\Mail\Transport\SmtpOptions();
+
+        		$smtpOptions->setHost('smtp.gmail.com')
+        		            ->setConnectionClass('login')
+        		            ->setName('smtp.gmail.com')
+        		            ->setConnectionConfig(array(
+        		            	'username' => 'anass.rakibi@gmail.com',
+        		            	'password' => '#############',
+        		            	'ssl'      => 'tls'
+	        		            ));
+
+	           $transport = new \Zend\Mail\Transport\Smtp($smtpOptions);
+	           $transport->send($message);
+
+
+
+        	}		
+		return array('form' => $contact_form);
+    }
+
+
+}
+
